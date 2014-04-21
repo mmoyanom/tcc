68beffe0d6209ceca49bc72d987e208e4f8e6595
Manuel Moyano <mnlmoyano@gmail.com>
2014-04-21 17:40:54 -0300

    Upload webservice

---
+++ webservice/ws/App_Code/DBConnection.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data.SqlClient
+Imports System.Configuration
+
+Public Class DBConnection
+
+    Public Shared Function GetConnection() As SqlConnection
+
+        Dim con As New SqlConnection
+        con.ConnectionString = ConfigurationManager.ConnectionStrings("MSSQL_connectionString").ConnectionString
+        Return con
+
+    End Function
+
+
+End Class
+
+
